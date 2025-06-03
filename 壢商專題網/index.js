b8fb4d44a0403cbe332a4e6c3a32844c0cfa4929
xshj0403 <214313@clvsc.tyc.edu.tw>
2025-06-03 23:17:50 +0800

    view5

--- 壢商專題網/index.js
+++ 壢商專題網/index.js
@@ -1,24 +1,49 @@
-const swiper = new Swiper('.swiper', {
-      slidesPerView: 3,
-      spaceBetween: 20,
-      loop: true,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-      },
-      breakpoints: {
-        992: {
-          slidesPerView: 3
-        },
-        768: {
-          slidesPerView: 2
-        },
-        0: {
-          slidesPerView: 1
-        }
-      }
+$(function () {
+  const $container = $('.elements');
+
+  for (let i = 0; i < 10; i++) {
+    const top = Math.random() * 100;
+    const left = Math.random() * 100;
+    const duration = 7 + Math.random() * 5;
+
+    const $div = $('<div></div>').css({
+      top: top + '%',
+      left: left + '%',
+      animation: `particles ${duration}s linear infinite`
     });
+
+    $container.append($div);
+  };
+});
+
+
+const swiper = new Swiper('.card-wrapper', {
+
+  loop: true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints:{
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    }
+  }
+});
