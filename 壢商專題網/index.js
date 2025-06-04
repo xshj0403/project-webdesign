6b7dd58f25f57174b53bc9d6ecf17085185fc84d
xshj0403 <214313@clvsc.tyc.edu.tw>
2025-06-05 00:09:21 +0800

    view6

--- 壢商專題網/index.js
+++ 壢商專題網/index.js
@@ -16,23 +16,87 @@ $(function () {
   };
 });
 
-
-const swiper = new Swiper('.card-wrapper', {
+// 商業經營科
+const swiper1 = new Swiper('.swiper1', {
 
   loop: true,
   spaceBetween: 25,
 
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: '.pagination1',
     clickable: true,
     dynamicBullets: true
   },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.next1',
+    prevEl: '.prev1',
+  },
+
+  breakpoints:{
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    }
+  }
+});
+
+// 國際貿易科
+const swiper2 = new Swiper('.swiper2', {
+
+  loop: true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: '.pagination2',
+    clickable: true,
+    dynamicBullets: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.next2',
+    prevEl: '.prev2',
+  },
+
+  breakpoints:{
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    }
+  }
+});
+
+// 國際貿易科
+const swiper3 = new Swiper('.swiper3', {
+
+  loop: true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: '.pagination3',
+    clickable: true,
+    dynamicBullets: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.next3',
+    prevEl: '.prev3',
   },
 
   breakpoints:{
